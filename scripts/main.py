d64f0cdd6159e34f584665a639e7079909f156a7
Paulpoomrit <paulpoomrit@gmail.com>
2026-04-05 21:23:22 -0700

    Added function for ablation

--- scripts/main.py
+++ scripts/main.py
@@ -8,8 +8,6 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_curve, roc_auc_score
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_moons
-
 
 data_path = 'data/test_data/w_features.tsv'
 feature_names = ["emdash",
@@ -30,60 +28,93 @@ feature_names = ["emdash",
                  "summary"
                  ]
 
+def train_model(df: pd.DataFrame, ablated_feature: str):
+
+    feature_list = feature_names
+
+    if ablated_feature in feature_list:
+        print(f'removing: {ablated_feature}')
+        feature_list.remove(ablated_feature)
+
+    y = df['label']
+    X = df[feature_list].values
+
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.2,
+                                                        random_state=104,
+                                                        shuffle=True)
+
+
+    model = RandomForestClassifier(
+        n_estimators=500, max_leaf_nodes=16, n_jobs=-1, random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    # print(y_pred)
+    y_pred_prob = model.predict_proba(X_test)[:, 1]
+
+    # # Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred)
+    # Accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    # Precision
+    precision = precision_score(y_test, y_pred, pos_label="AI")
+    # Recall
+    recall = recall_score(y_test, y_pred, pos_label="AI")
+    # F1-Score
+    f1 = f1_score(y_test, y_pred, pos_label="AI")
+    # # ROC Curve and AUC
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob, pos_label="AI")
+    roc_auc = roc_auc_score(y_test, y_pred_prob)
+
+    # # # Plot the ROC curve
+    # # plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    # # # roc curve for tpr = fpr
+    # # plt.plot([0, 1], [0, 1], 'k--', label='Random classifier')
+    # # plt.xlabel('False Positive Rate')
+    # # plt.ylabel('True Positive Rate')
+    # # plt.title('ROC Curve')
+    # # plt.legend(loc="lower right")
+    # # plt.show()
+
+    # # # Plot the predicted class probabilities
+    # # plt.hist(y_pred_prob, bins=10)
+    # # plt.xlim(0, 1)
+    # # plt.title('Histogram of predicted probabilities')
+    # # plt.xlabel('Predicted probability of AI')
+    # # plt.ylabel('Frequency')
+    # # plt.show()
+
+
+    print(f"Accuracy:  {accuracy:.2f}")
+    print(f"Precision: {precision:.2f}")
+    print(f"Recall:    {recall:.2f}")
+    print(f"F1 Score:  {f1:.2f}")
+    print(f"AUROC:  {roc_auc:.2f}")
+
+    print("\n---Feature importance---\n")
+    for score, name in zip(model.feature_importances_, df.columns):
+        print(f"| {name} | {round(score,2)} |")
+    print("\n------------------------\n")
+
+
+
 
 df = pd.read_csv(data_path, sep='\t', index_col=False)
 df = df.dropna()
 
-y = df['label']
-X = df[feature_names].values
-
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.2,
-                                                    shuffle=True)
+print("\n----------------------------------------\n")
+print(f"All features")
+train_model(df, 'none')
+print("\n----------------------------------------\n")
 
 
-model = RandomForestClassifier(
-    n_estimators=500, max_leaf_nodes=16, n_jobs=-1, random_state=42)
-model.fit(X_train, y_train)
+for feature in feature_names:
+    print("\n----------------------------------------\n")
+    print(f"Ablated feature: {feature}")
+    ablated_df = df.copy()
+    ablated_df = ablated_df.drop(feature, axis=1)
+    train_model(ablated_df, feature)
+    print("\n----------------------------------------\n")
 
-y_pred = model.predict(X_test)
-y_pred_prob = model.predict_proba(X_test)[:, 1]
-
-# # Confusion Matrix
-cm = confusion_matrix(y_test, y_pred)
-# Accuracy
-accuracy = accuracy_score(y_test, y_pred)
-# Precision
-precision = precision_score(y_test, y_pred, pos_label="AI")
-# Recall
-recall = recall_score(y_test, y_pred, pos_label="AI")
-# F1-Score
-f1 = f1_score(y_test, y_pred, pos_label="AI")
-# # ROC Curve and AUC
-fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob, pos_label="AI")
-roc_auc = roc_auc_score(y_test, y_pred_prob)
-
-# # Plot the ROC curve
-# plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-# # roc curve for tpr = fpr
-# plt.plot([0, 1], [0, 1], 'k--', label='Random classifier')
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('ROC Curve')
-# plt.legend(loc="lower right")
-# plt.show()
-
-# # Plot the predicted class probabilities
-# plt.hist(y_pred_prob, bins=10)
-# plt.xlim(0, 1)
-# plt.title('Histogram of predicted probabilities')
-# plt.xlabel('Predicted probability of AI')
-# plt.ylabel('Frequency')
-# plt.show()
-
-
-print(f"Accuracy:  {accuracy:.2f}")
-print(f"Precision: {precision:.2f}")
-print(f"Recall:    {recall:.2f}")
-print(f"F1 Score:  {f1:.2f}")
-print(f"AUROC:  {roc_auc:.2f}")
